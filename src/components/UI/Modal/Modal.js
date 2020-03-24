3956674de87b529ddaf6ea752116e33aa9ec428e
ilkerkaran <ilkerkaran@gmail.com>
2020-03-24 16:03:39 +0000

    performance optimised

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Modal.css';
 import PropTypes from 'prop-types';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <>
-    <Backdrop show={props.show} onClick={props.cancelPurchasing} />
-    <div
-      className="Modal"
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </>
-);
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default modal;
+  shouldComponentUpdate(nextprops) {
+    return nextprops.show !== this.props.show;
+  }
 
-modal.propTypes = {
+  render() {
+    return (
+      <>
+        <Backdrop
+          show={this.props.show}
+          onClick={this.props.cancelPurchasing}
+        />
+        <div
+          className="Modal"
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
+export default Modal;
+
+Modal.propTypes = {
   show: PropTypes.bool.isRequired,
   cancelPurchasing: PropTypes.func.isRequired
 };
