8fec54749d1a3d1d890b58bd15bb2804ad98570f
ilkerkaran <ilkerkaran@gmail.com>
2020-04-11 05:32:36 +0100

    service xied to return promise

--- src/services/ingredientsService.js
+++ src/services/ingredientsService.js
@@ -1,12 +1,4 @@
 import axios from '../axios-orders';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getIngredientsWithPrice = async () => {
-  try {
-    const { data: priceData } = await axios.get('/ingredient-prices.json');
-    return priceData;
-  } catch (err) {
-    console.log('error while executing getIngredientsWithPrice; ', err);
-  }
-  return {};
-};
+export const getIngredientsWithPrice = () => axios.get('/ingredient-prices.json');
