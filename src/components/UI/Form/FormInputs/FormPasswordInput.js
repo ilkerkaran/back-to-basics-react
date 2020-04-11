b74ec7873341a2c4caa10e49b4d28ae26d60c010
ilkerkaran <ilkerkaran@gmail.com>
2020-04-11 02:07:12 +0100

    PasswordInput Component added

---
+++ src/components/UI/Form/FormInputs/FormPasswordInput.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { inputConfigTypes } from '../../../../propTypes/types';
+
+const email = ({ className, inputConfig }) => (<div className={className}>
+  <input type="password"
+    className="Input"
+    placeholder={inputConfig.label}
+    onChange={inputConfig.onChange}
+    name={inputConfig.inputName}
+    ref={inputConfig.isRequired ? inputConfig.register({
+      required: `${inputConfig.label} is required`,
+      pattern: {
+        value: /[0-9a-zA-Z]{6,}/,
+        message: 'invalid email address'
+      }
+    }) : inputConfig.register({
+      pattern: {
+        value: /[0-9a-zA-Z]{6,}/,
+        message: 'invalid email address'
+      }
+    })}/>
+    <div style={{ color: 'darkred' }}>
+    {inputConfig.errors[inputConfig.inputName] && inputConfig.errors[inputConfig.inputName].message}
+    </div>
+</div>);
+
+export default email;
+
+email.propTypes = {
+  className: PropTypes.string,
+  ...inputConfigTypes
+};
