b97e6a279b7ac0363e5994e70340b6cf1ddef44f
ilkerkaran <ilkerkaran@gmail.com>
2020-04-11 05:33:45 +0100

    auth routes added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Checkout/Orders/Orders';
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout';
 
-function App() {
+function App({ isAuthenticated }) {
+  const securedRoutes = (<>
+    <Route path="/checkout" component={Checkout} />
+    <Route path="/orders" component={Orders} />
+    <Route path="/logout" component={Logout} />
+  </>);
+
+  const anonymousRoutes = (<>
+    <Route path="/checkout" component={Checkout} />
+    <Route path="/signin" render={() => <Auth isSignUp={false}/>}/>
+    <Route path="/signup" render={() => <Auth isSignUp={true}/>}/>
+  </>);
   return (
     <div>
       <Layout>
         <Redirect path="/" exact to="burgerBuilder" />
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/orders" component={Orders} />
+        {isAuthenticated ? securedRoutes : anonymousRoutes}
         <Route path="/burgerBuilder" exact component={BurgerBuilder} />
       </Layout>
     </div>
   );
 }
+const mapStatetoProps = (state) => ({
 
-export default App;
+  isAuthenticated: !!state.auth.token
+});
+export default connect(mapStatetoProps)(App);
+
+App.propTypes = {
+  isAuthenticated: PropTypes.bool
+
+};
