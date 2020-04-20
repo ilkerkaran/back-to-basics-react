7f3eec3ba3a824c4831195076be4c1f53a6f9b22
ilkerkaran <ilkerkaran@gmail.com>
2020-04-20 16:34:30 +0100

    added Lazy loading and converted redux hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect, Suspense } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Checkout/Orders/Orders';
-import Auth from './containers/Auth/Auth';
-import Logout from './containers/Auth/Logout';
 import * as actions from './store/actions/actionCreators';
 
-const app = ({ isAuthenticated, checkIsAuthenticated }) => {
-  const securedRoutes = (<>
+const Logout = React.lazy(() => import('./containers/Auth/Logout'));
+const Orders = React.lazy(() => import('./containers/Checkout/Orders/Orders'));
+const Auth = React.lazy(() => import('./containers/Auth/Auth'));
+const Checkout = React.lazy(() => import('./containers/Checkout/Checkout'));
+
+const app = () => {
+  const isAuthenticated = useSelector((state) => !!state.auth.token);
+  const dispatch = useDispatch();
+  const checkIsAuthenticated = () => dispatch(actions.checkIsAuthenticated());
+  const securedRoutes = (
+    <Suspense fallback={<div>Loading...</div>}>
     <Route path="/checkout" component={Checkout} />
     <Route path="/orders" component={Orders} />
     <Route path="/logout" component={Logout} />
-  </>);
+    </Suspense>);
 
-  const anonymousRoutes = (<>
+  const anonymousRoutes = (<Suspense fallback={<div>Loading...</div>}>
     <Route path="/signin" render={() => <Auth isSignUp={false}/>}/>
     <Route path="/signup" render={() => <Auth isSignUp={true}/>}/>
-  </>);
+    </Suspense>);
 
   useEffect(() => {
     checkIsAuthenticated();
@@ -37,15 +42,7 @@ const app = ({ isAuthenticated, checkIsAuthenticated }) => {
     </div>
   );
 };
-const mapStatetoProps = (state) => ({
-
-  isAuthenticated: !!state.auth.token
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  checkIsAuthenticated: () => dispatch(actions.checkIsAuthenticated())
-});
-export default connect(mapStatetoProps, mapDispatchToProps)(app);
+export default app;
 
 app.propTypes = {
   isAuthenticated: PropTypes.bool,
